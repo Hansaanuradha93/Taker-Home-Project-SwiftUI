c29cc4e78c3ad40a043f65239264051825ab4895
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2023-02-09 20:51:52 +0530

    assets symbols added

---
+++ Take Home Project SwiftUI/Resources/Symbols.swift
@@ -0,0 +1,15 @@
+//
+//  Symbols.swift
+//  Take Home Project SwiftUI
+//
+//  Created by Hansa Anuradha on 2023-02-09.
+//
+
+import SwiftUI
+
+enum Symbols {
+    static let plus = Image(systemName: "plus")
+    static let person = Image(systemName: "person.2")
+    static let gear = Image(systemName: "gear")
+}
+

05fb71f75848785ca273ffc1338ff1294997c665
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2023-02-09 21:00:43 +0530

    colours added

---
+++ Take Home Project SwiftUI/Resources/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  Take Home Project SwiftUI
+//
+//  Created by Hansa Anuradha on 2023-02-09.
+//
+
+import SwiftUI
+
+enum Theme {
+    static let background = Color("background")
+    static let detailBackground = Color("detail-background")
+    static let pill = Color("pill")
+    static let text = Color("text")
+}

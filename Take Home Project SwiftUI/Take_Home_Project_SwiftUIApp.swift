967506d5cdcff60143f4bac75e034f142274b668
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2023-02-09 20:12:52 +0530

    project files added

---
+++ Take Home Project SwiftUI/Take_Home_Project_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Take_Home_Project_SwiftUIApp.swift
+//  Take Home Project SwiftUI
+//
+//  Created by Hansa Anuradha on 2023-02-09.
+//
+
+import SwiftUI
+
+@main
+struct Take_Home_Project_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

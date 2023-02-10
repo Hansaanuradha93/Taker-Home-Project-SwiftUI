8f487b51a8580387eed37af2b5339d9e77537c62
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2023-02-10 16:00:00 +0530

    launch screen added

--- Take Home Project SwiftUI/Take_Home_Project_SwiftUIApp.swift
+++ Take Home Project SwiftUI/Take_Home_Project_SwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Take_Home_Project_SwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//            ContentView()
         }
     }
 }

248c4cc06d69bcaeb8519952cc03331ad394ce5a
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2023-02-11 20:36:19 +0530

    People View designed and code refactored

---
+++ Take Home Project SwiftUI/Features/People/Views/PersonItemView.swift
@@ -0,0 +1,52 @@
+//
+//  PersonItemView.swift
+//  Take Home Project SwiftUI
+//
+//  Created by Hansa Anuradha on 2023-02-11.
+//
+
+import SwiftUI
+
+struct PersonItemView: View {
+    
+    let user: Int
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            
+            Rectangle()
+                .fill(.blue)
+                .frame(height: 130)
+            
+            VStack(alignment: .leading) {
+                VStack {
+                    
+                    Text("#\(user)")
+                        .font(.system(.caption, design: .rounded, weight: .bold))
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 9)
+                        .padding(.vertical, 4)
+                        .background(Theme.pill, in: Capsule())
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Text("<First name> <Last name>")
+                    .foregroundColor(Theme.text)
+                    .font(.system(.body, design: .rounded))
+            }
+            .padding(.horizontal, 8)
+            
+            
+        }
+        .background(Theme.detailBackground)
+        .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+        .shadow(color: Theme.text.opacity(0.1), radius: 2, x: 0, y: 1)
+        .padding(.vertical, 5)
+    }
+}
+
+struct PersonItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        PersonItemView(user: 5)
+    }
+}

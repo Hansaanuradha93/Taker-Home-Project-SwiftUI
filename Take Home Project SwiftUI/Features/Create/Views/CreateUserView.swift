e89071cb4a8f4b8fe92ec26ad7c069607a4072de
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2023-02-17 17:51:16 +0530

    createview added

---
+++ Take Home Project SwiftUI/Features/Create/Views/CreateUserView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateUserView.swift
+//  Take Home Project SwiftUI
+//
+//  Created by Hansa Anuradha on 2023-02-17.
+//
+
+import SwiftUI
+
+struct CreateUserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreateUserView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateUserView()
+    }
+}
---
+++ Take Home Project SwiftUI/Features/Create/Views/CreateView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateView.swift
+//  Take Home Project SwiftUI
+//
+//  Created by Hansa Anuradha on 2023-02-17.
+//
+
+import SwiftUI
+
+struct CreateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreateView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateView()
+    }
+}

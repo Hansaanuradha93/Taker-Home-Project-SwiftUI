8def11c9da9765b5e14159008e2142e597b8cb2a
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2023-02-21 22:38:00 +0530

    NetworkManager code cleaned

--- Take Home Project SwiftUI/Base/API/NetworkManager.swift
+++ Take Home Project SwiftUI/Base/API/NetworkManager.swift
@@ -20,7 +20,6 @@ extension NetworkManager {
     /// Request api and resturns decoded data
     /// - Parameters:
     ///   - endPoint: Endpoint url
-    ///   - httpMethod: Http method type
     ///   - type: Response decoding type
     ///   - completion: Returns decoded json data or error
     func request<T: Codable>(endPoint: EndPoint,
@@ -75,7 +74,6 @@ extension NetworkManager {
     /// Request api and returns the result
     /// - Parameters:
     ///   - endPoint: Endpoint url
-    ///   - httpMethod: Http method type
     ///   - completion: Returns status or error
     func request(endPoint: EndPoint,
                  completion: @escaping (Result<Void, Error>) -> Void) {

df729eba04beb99f246ec75d860aa186063a232f
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2023-02-25 15:33:11 +0530

    JSON Mapper unit tested

--- Take Home Project SwiftUI/Base/Mapper/StaticJSONMapper.swift
+++ Take Home Project SwiftUI/Base/Mapper/StaticJSONMapper.swift
@@ -11,28 +11,21 @@ struct StaticJSONMapper {
     
     static func decode<T: Decodable>(file: String, type: T.Type) throws -> T {
         
-        guard let path = Bundle.main.path(forResource: file, ofType: "json"),
+        guard !file.isEmpty,
+              let path = Bundle.main.path(forResource: file, ofType: "json"),
               let data = FileManager.default.contents(atPath: path) else {
-            throw MappingError.failedToNavigateTOJSONFile
+            throw MappingError.failedToGetContent
         }
         
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
-
-        do {
-            let result = try decoder.decode(type, from: data)
-            return result
-        } catch (let error) {
-            print("‼️ static JSON decoding error: \(error)")
-            throw MappingError.decodingError
-        }
+        return try decoder.decode(type, from: data)
     }
 }
 
 extension StaticJSONMapper {
     
     enum MappingError: Error {
-        case failedToNavigateTOJSONFile
-        case decodingError
+        case failedToGetContent
     }
 }
--- Take Home Project SwiftUITests/Mapper/JSONMapperTests.swift
+++ Take Home Project SwiftUITests/Mapper/JSONMapperTests.swift
@@ -7,8 +7,76 @@
 
 import Foundation
 import XCTest
+@testable import Take_Home_Project_SwiftUI
 
 class JSONMapperTests: XCTestCase {
     
+    func test_with_valid_json_successfully_decodes() {
+        
+        XCTAssertNoThrow(try StaticJSONMapper.decode(file: "UsersStaticData", type: UsersResponse.self), "Mapper shouldn't throw and error")
+        
+        let usersResponse = try? StaticJSONMapper.decode(file: "UsersStaticData", type: UsersResponse.self)
+        XCTAssertNotNil(usersResponse, "Users respoonse shouldn't be nil")
+        
+        XCTAssertEqual(usersResponse?.page, 1, "Page number should be 1")
+        XCTAssertEqual(usersResponse?.perPage, 6, "Per page should be 6")
+        XCTAssertEqual(usersResponse?.total, 12, "Total should be 12")
+        XCTAssertEqual(usersResponse?.totalPages, 2, "Total pages should be 2")
+        
+        XCTAssertEqual(usersResponse?.data.count, 6, "The total number of users should be 6")
+        
+        let firstUser = usersResponse?.data[0]
+        
+        XCTAssertEqual(firstUser?.id, 1, "Id should be 1")
+        XCTAssertEqual(firstUser?.email, "george.bluth@reqres.in", "Email should be george.bluth@reqres.in")
+        XCTAssertEqual(firstUser?.firstName, "George", "first name should be George")
+        XCTAssertEqual(firstUser?.lastName, "Bluth", "Last name should be Bluth")
+        XCTAssertEqual(firstUser?.avatar, "https://reqres.in/img/faces/1-image.jpg", "The url should be https://reqres.in/img/faces/1-image.jpg")
+    }
     
+    func test_with_missing_file_name_error_thrown() {
+        XCTAssertThrowsError(try StaticJSONMapper.decode(file: "", type: UsersResponse.self), "An error should be thrown")
+        
+        do {
+            _ = try StaticJSONMapper.decode(file: "", type: UsersResponse.self)
+            
+        } catch {
+            guard let mappingError = error as? StaticJSONMapper.MappingError else {
+                XCTFail("This is the wrong type of error")
+                return
+            }
+            
+            XCTAssertEqual(mappingError, StaticJSONMapper.MappingError.failedToGetContent, "This should be failed to get content error")
+        }
+    }
+    
+    func test_with_invalid_file_name_error_thrown() {
+        XCTAssertThrowsError(try StaticJSONMapper.decode(file: "jdnsf", type: UsersResponse.self), "An error should be thrown")
+        
+        do {
+            _ = try StaticJSONMapper.decode(file: "jdnsf", type: UsersResponse.self)
+            
+        } catch {
+            guard let mappingError = error as? StaticJSONMapper.MappingError else {
+                XCTFail("This is the wrong type of error")
+                return
+            }
+            
+            XCTAssertEqual(mappingError, StaticJSONMapper.MappingError.failedToGetContent, "This should be failed to get content error")
+        }
+    }
+    
+    func test_with_invalid_json_error_thrown() {
+        
+        XCTAssertThrowsError(try StaticJSONMapper.decode(file: "UsersStaticData", type: UserDetailsResponse.self), "An error should be thrown")
+        
+        do {
+            _ = try StaticJSONMapper.decode(file: "UsersStaticData", type: UserDetailsResponse.self)
+            
+        } catch {
+            if error is StaticJSONMapper.MappingError {
+                XCTFail("Got the wrong type of error, expecting a system decoding error")
+            }
+        }
+    }
 }

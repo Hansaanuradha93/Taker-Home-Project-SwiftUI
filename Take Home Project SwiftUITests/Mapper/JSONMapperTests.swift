08dafc5a6cb66cad92ed191f13eb61061bbec372
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2023-02-25 14:11:57 +0530

    test target added

---
+++ Take Home Project SwiftUITests/Mapper/JSONMapperTests.swift
@@ -0,0 +1,14 @@
+//
+//  JSONMapperTests.swift
+//  Take Home Project SwiftUITests
+//
+//  Created by Hansa Anuradha on 2023-02-25.
+//
+
+import Foundation
+import XCTest
+
+class JSONMapperTests: XCTestCase {
+    
+    
+}
